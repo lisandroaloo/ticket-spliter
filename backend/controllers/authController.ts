41388461f1c8edd5b2487bea58d5640b0ee278c2
Franco-Di-Silvestro <francodisilvestro2000@gmail.com>
2024-09-02 16:03:36 -0300

    log out

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -1,70 +1,57 @@
-import bcrypt from "bcryptjs"
-import prisma from "../models/prismaClient";
-import generateTokenAndSetCookie from "../utils/generateTokenAndSetCookie";
-
+import bcrypt from 'bcryptjs'
+import prisma from '../models/prismaClient'
+import generateTokenAndSetCookie from '../utils/generateTokenAndSetCookie'
 
 export const signUp = async (req: any, res: any) => {
-    try {
-        const { us_nombre, us_email, us_password, confirmPassword } = req.body
-        
-        if (us_password != confirmPassword) {
-            return res.status(400).json({ error: "Passwords don't match" })
-        }
+  try {
+    const { us_nombre, us_email, us_password, confirmPassword } = req.body
 
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(us_password, salt)
-
-        const newUser = await prisma.usuario.create({
-          data: { us_nombre, us_email, us_password: hashedPassword, us_role:"usuario_general", us_estado:"Activo" },
-        })
-        res.json(newUser)
-
-
-    } catch (error: any) {
-        console.error("Error signup controller", error.message)
-        res.status(500).json({ error: "internal Server Error" });
+    if (us_password != confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" })
     }
 
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(us_password, salt)
+
+    const newUser = await prisma.usuario.create({
+      data: { us_nombre, us_email, us_password: hashedPassword, us_role: 'usuario_general', us_estado: 'Activo' },
+    })
+    res.json(newUser)
+  } catch (error: any) {
+    console.error('Error signup controller', error.message)
+    res.status(500).json({ error: 'internal Server Error' })
+  }
 }
 
-
 export const login = async (req: any, res: any) => {
-    try {
-        const { us_email, us_password: pw } = req.body
+  try {
+    const { us_email, us_password: pw } = req.body
 
-        const user = await prisma.usuario.findUnique({ where: { us_email} })
+    const user = await prisma.usuario.findUnique({ where: { us_email } })
 
-        if (user) {
-            const { us_password } = user;
-            const passwordMatch = await bcrypt.compare(pw, us_password)
-            if (passwordMatch) {
-                generateTokenAndSetCookie(us_email, res)
-            }
-
-        } else {
-            throw Error("No se encontro el usuario")
-        }
-
-
-        res.json(user)
-
-    } catch (error: any) {
-        console.error("Error login controller", error.message)
-        res.status(500).json({ error: "internal Server Error" });
+    if (user) {
+      const { us_password } = user
+      const passwordMatch = await bcrypt.compare(pw, us_password)
+      if (passwordMatch) {
+        generateTokenAndSetCookie(us_email, res)
+      }
+    } else {
+      throw Error('No se encontro el usuario')
     }
 
-
+    res.json(user)
+  } catch (error: any) {
+    console.error('Error login controller', error.message)
+    res.status(500).json({ error: 'internal Server Error' })
+  }
 }
 
 export const logout = async (req: any, res: any) => {
-    try {
-        
-
-
-    } catch (error: any) {
-        console.error("Error logout controller", error.message)
-        res.status(500).json({ error: "internal Server Error" });
-    }
-
-
+  try {
+    res.cookie('jwt', '', { maxAge: 0 })
+    res.status(200).json({ message: 'Logged out successfully' })
+  } catch (error: any) {
+    console.error('Error logout controller', error.message)
+    res.status(500).json({ error: 'internal Server Error' })
+  }
 }
