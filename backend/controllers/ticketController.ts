2596f090fde877b98d3ee8d25440062f9e322d3c
Franco-Di-Silvestro <francodisilvestro2000@gmail.com>
2024-11-23 11:22:24 -0300

    email por tickets

--- backend/controllers/ticketController.ts
+++ backend/controllers/ticketController.ts
@@ -1,41 +1,34 @@
-import prisma from "../models/prismaClient"
+import prisma from '../models/prismaClient'
+import { sendMail } from '../services/mail'
 
 export const createTicket = async (req: any, res: any) => {
   try {
     const { us_email, pr_id, ti_monto, ti_descripcion, ti_fecha, ti_image_url, userPercentage, ti_id } = req.body
 
-    const ticket = ti_id ? 
-
-      await prisma.ticket.update({
-        where: {
-          ti_id
-        },
-        data: {
-          ti_us_id: us_email,
-          ti_pr_id: +pr_id,
-          ti_monto: +ti_monto,
-          ti_descripcion: ti_descripcion,
-          ti_fecha: ti_fecha,
-          ti_image_url: ti_image_url
-        },
-    })
-
-     : 
-
-      await prisma.ticket.create({
-        data: {
-          ti_us_id: us_email,
-          ti_pr_id: +pr_id,
-          ti_monto: +ti_monto,
-          ti_descripcion: ti_descripcion,
-          ti_fecha: ti_fecha,
-          ti_image_url: ti_image_url
-        },
-      })
-    
-
- 
-    
+    const ticket = ti_id
+      ? await prisma.ticket.update({
+          where: {
+            ti_id,
+          },
+          data: {
+            ti_us_id: us_email,
+            ti_pr_id: +pr_id,
+            ti_monto: +ti_monto,
+            ti_descripcion: ti_descripcion,
+            ti_fecha: ti_fecha,
+            ti_image_url: ti_image_url,
+          },
+        })
+      : await prisma.ticket.create({
+          data: {
+            ti_us_id: us_email,
+            ti_pr_id: +pr_id,
+            ti_monto: +ti_monto,
+            ti_descripcion: ti_descripcion,
+            ti_fecha: ti_fecha,
+            ti_image_url: ti_image_url,
+          },
+        })
 
     const usuariosConPorcentajePromises = userPercentage.map(async (usuario: any) => {
       ti_id
@@ -57,7 +50,35 @@ export const createTicket = async (req: any, res: any) => {
               uxt_porcentaje: +usuario.percentage, // El porcentaje que debe pagar este usuario
             },
           })
-    });
+    })
+
+    const usuariosEnElProyecto = await prisma.usuario.findMany(
+      {
+        where: {
+          UsuarioXProyecto: {
+            some: {
+              uxp_pr_id: +pr_id
+            }
+          }
+        }
+      }
+    )
+
+    const _proyecto = await prisma.proyecto.findUnique({where: {pr_id: +pr_id}, select:{pr_nombre: true}})
+
+    ti_id
+      ? usuariosEnElProyecto.forEach((us) => {
+          sendMail({
+            email: us.us_email,
+            subject: `Se edito un ticket en el proyecto ${_proyecto!.pr_nombre}`,
+            htmlTemplate: `Hola, se edito el ticket ${ti_id} en el proyecto ${_proyecto!.pr_nombre}`,
+          })
+        })
+      : usuariosEnElProyecto.forEach((us) => {sendMail({
+          email: us.us_email,
+          subject: `Se añadio un ticket en el proyecto ${_proyecto!.pr_nombre}`,
+          htmlTemplate: `Hola, se añadio un ticket de $${ti_monto} en el proyecto ${_proyecto!.pr_nombre}`,
+        })})
 
     await Promise.all(usuariosConPorcentajePromises)
 
