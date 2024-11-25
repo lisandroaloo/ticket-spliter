618ad148670e09194fdde655bd198c192ddb9db4
Franco-Di-Silvestro <francodisilvestro2000@gmail.com>
2024-11-25 00:07:10 -0300

    pagos usuario

--- frontend/src/components/profile/PaymentCards.tsx
+++ frontend/src/components/profile/PaymentCards.tsx
@@ -3,15 +3,19 @@ import { IPaymentCards } from '../../../interfaces'
 
 
 const PaymentCards = ({ pagos, emisor }: IPaymentCards) => {
+    
+    
     return (
-        <>
-
-            {pagos.map((pr) => (
-                <div className="rounded-lg p-2 mb-1 shadow-sm bg-green-700">
-                    <div key={pr.pa_id}>{(emisor ? pr.Emisor?.us_nombre : pr.Receptor?.us_nombre) + ' - ' + pr.pa_fecha.split('T')[0] + ' - $' + pr.pa_monto + " - " + pr.pa_isRecibido ? "" : "⌛"}</div>
-                </div>
-            ))}
-        </>
+      <>
+        {pagos.map((pr, index) => (
+          <div
+            key={index}
+            className="rounded-lg p-2 mb-1 shadow-sm bg-green-700"
+          >
+            <div>{(emisor ? pr.Emisor?.us_nombre : pr.Receptor?.us_nombre) + ' - ' + pr.pa_fecha.split('T')[0] + ' - $' + pr.pa_monto + (pr.pa_isRecibido ? '' : ' - ⌛')}</div>
+          </div>
+        ))}
+      </>
     )
 }
 
--- frontend/src/components/profile/TicketCards.tsx
+++ frontend/src/components/profile/TicketCards.tsx
@@ -3,14 +3,16 @@ import { IProfileTickets } from '../../../interfaces'
 
 const TicketCards = ({ tickets }: IProfileTickets) => {
     return (
-        <>
-
-            {tickets.map((t) => (
-                <div className="rounded-lg p-2 mb-1 shadow-sm bg-green-700">
-                    <div key={t.ti_id}>{t.ti_descripcion + ' - ' + t.ti_fecha.split('T')[0] + ' - $' + t.ti_monto}</div>
-                </div>
-            ))}
-        </>
+      <>
+        {tickets.map((t, index) => (
+          <div
+            key={index}
+            className="rounded-lg p-2 mb-1 shadow-sm bg-green-700"
+          >
+            <div>{t.ti_descripcion + ' - ' + t.ti_fecha.split('T')[0] + ' - $' + t.ti_monto}</div>
+          </div>
+        ))}
+      </>
     )
 }
 
